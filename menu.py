d08bd63618dff081acef19f55345b9345de4deb6
MuslimMama <134977306+MuslimMama@users.noreply.github.com>
2025-08-05 15:17:20 -0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,18 +2,19 @@ import sh1106
 from machine import Pin, I2C
 import time
 
-button = Pin(18, Pin.IN, Pin.PULL_UP)
-menubutton = Pin(19, Pin.IN, Pin.PULL_UP)
-
-i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=800_000)
+selectbutton = Pin(20, Pin.IN, Pin.PULL_UP)
+menuselectbutton = Pin(19, Pin.IN, Pin.PULL_UP)
+i2c = I2C(0, scl=Pin(10), sda=Pin(9), freq=800_000)
 oled = sh1106.SH1106_I2C(128, 64, i2c, res=Pin(2), addr=0x3C, delay=100)
+
+
 
 def menu():
     
     checker = False
     
     
-    if menubutton.value() == 0:
+    if menuselectbutton.value() == 0:
         oled.fill(0) #fills screen with black to go into menu screen
         counter = 0 #used just to keep track of what row we're on
         
@@ -25,11 +26,11 @@ def menu():
             oled.show()
             
             #breaks while loop if cursor is next to exit, making us go back to the default kitty
-            if button.value() == 0 and counter == 3: #  3 is the position where it's at "exit"
+            if selectbutton.value() == 0 and counter == 3: #  3 is the position where it's at "exit"
                 print("leave")
                 break
             
-            elif button.value() == 0 and counter == 1:
+            elif selectbutton.value() == 0 and counter == 1:
                 oled.fill(0) #fills screen with black to go into temps screen
                 oled.text("<", 63, 10) #first instance of the indicator
                 while True:
@@ -39,13 +40,13 @@ def menu():
                     oled.text("Exit", 10, 40)
                     oled.show()
                     
-                    if button.value() == 0 and counter == 4: #  3 is the position where it's at "exit"
+                    if selectbutton.value() == 0 and counter == 4: #  3 is the position where it's at "exit"
                         print("leave")
                         oled.fill(0)
                         oled.text("<", 55, 10) # needs a first instance of the indicator
                         break
                     
-                    if menubutton.value() == 0:
+                    if menuselectbutton.value() == 0:
                         oled.fill_rect(63, 0, 20, 64, 0)
                         counter = counter %4
                         oled.text("<", 63, 10 + (counter * 10)) #position indicator
@@ -54,7 +55,7 @@ def menu():
                         oled.show()
                         time.sleep(0.15) #debouncing
                         
-            if menubutton.value() == 0:
+            if menuselectbutton.value() == 0:
                 oled.fill_rect(55, 0, 20, 64, 0)
                 
                 counter = counter %3 #loops into being either 1, 2, 3 b/c 3 positions available
@@ -64,4 +65,5 @@ def menu():
                 time.sleep(0.15) #debouncing
     return None
  
+
 
