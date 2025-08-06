c826e0a4fd96b5f0b1c3478c7dd2eb6865f21c82
MuslimMama <134977306+MuslimMama@users.noreply.github.com>
2025-08-05 22:54:47 -0700

    Update blinker.py

--- blinker.py
+++ blinker.py
@@ -3,17 +3,15 @@ import sh1106
 import framebuf
 import time
 import penjipal
+import init
 
-penbutton = Pin(18, Pin.IN, Pin.PULL_UP)
-i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=800_000)
-oled = sh1106.SH1106_I2C(128, 64, i2c, res=Pin(2), addr=0x3C, delay=100)
-pal = penjipal.penjiPal(26)
+i2c, penbutton, menubutton, selectbutton, oled, pal = init.init()
 
 kitty = pal.get_cat_fb()
 kittycough = pal.get_cough_fb()
 kittycough2 = pal.get_cough2_fb()
 
-def blinker(kitty, kittycough, kittycough2, life_bar):
+def blinker(pal, kitty, kittycough, kittycough2, life_bar):
     #show default kitty, always reset life to 26, set init_life to 26, set default result to false
     oled.blit(kitty, 0, 0)
     pal.set_life(26)
@@ -28,7 +26,7 @@ def blinker(kitty, kittycough, kittycough2, life_bar):
 
     oled.show()
     # need to add third button, when button1 pressed, goes through while statement and reaches the 0.5s time sleep when exiting menu
-    while button.value() == 0:  # Button is pressed (active low)
+    while penbutton.value() == 0:  # Button is pressed (active low)
         sprite = not sprite  # Toggle animation frame
         pal.update_life()  # Reduce HP
         life = pal.get_life()
@@ -51,9 +49,10 @@ def blinker(kitty, kittycough, kittycough2, life_bar):
         
         if life_bar == 0:
             result = True
-            
+            break
     #reset all values after each press        
     init_life = pal.set_life(26)
     life_bar = 128  # Full width of display
     
     return result
+
